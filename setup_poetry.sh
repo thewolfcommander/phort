2f8e3b642bed4c85440a1284e40f194b899f6533
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-12-26 15:09:58 +0530

    v1.0.0/feat: Passed boilerplate_choice to poetry file

--- setup_poetry.sh
+++ setup_poetry.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Script for setting up Python environment using Poetry
 
+# Receive boilerplate choice as an argument
+boilerplate_choice=$1
+
 echo "Installing Poetry..."
 # Add the commands to install and configure Poetry here
 

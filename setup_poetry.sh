2574ea9ac16e39b45ffb6dcc4b8d4a17a83796a9
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-12-26 14:58:49 +0530

    v1.0.0/feat: Added Poetry and Virtualenv setup files

---
+++ setup_poetry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script for setting up Python environment using Poetry
+
+echo "Installing Poetry..."
+# Add the commands to install and configure Poetry here
+
+echo "Creating a new Poetry environment..."
+# Add the commands to create a new Poetry environment here
---
+++ setup_virtualenv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Script for setting up Python environment using Virtualenv
+
+echo "Installing Virtualenv..."
+# Add the commands to install and configure Virtualenv here
+
+echo "Creating a new Virtualenv environment..."
+# Add the commands to create a new Virtualenv environment here

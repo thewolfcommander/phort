11d5ce2f84702e344766810c3a537c452b5e8f33
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2023-12-26 23:51:50 +0530

    v1.0.1/feat: Complete execute.sh for all Django Boilerplate

---
+++ boilerplates/4/execute.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+# Validate input arguments
+if [ $# -lt 2 ]; then
+    echo "Usage: $0 <project_dir> <project_name>"
+    exit 1
+fi
+
+project_dir=$1
+project_name=$2
+
+# Navigate to the project directory
+mkdir -p "$project_dir"
+cd "$project_dir"
+
+# Check if Django is installed
+if ! python -m django --version; then
+    echo "Django is not installed. Please install Django first."
+    exit 1
+fi
+
+# Check if PostgreSQL is installed
+if ! command -v psql > /dev/null; then
+    echo "PostgreSQL is not installed. Please install PostgreSQL first."
+    exit 1
+fi
+
+# Create a virtual environment (optional but recommended)
+python -m venv venv
+
+# Determine OS and activate the virtual environment
+case "$(uname -s)" in
+    Linux*|Darwin*)
+        source venv/bin/activate
+        ;;
+    MINGW*|CYGWIN*|MSYS*)
+        .\venv\Scripts\activate
+        ;;
+    *)
+        echo "Unsupported OS for automatic activation of virtual environment"
+        exit 1
+        ;;
+esac
+
+# Step 1: Generate Django Project
+django-admin startproject $project_name .
+
+# Change to the newly created project directory
+cd $project_name
+
+# Generate a random secret key for Django
+DJANGO_SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+
+# Step 2: Create .env with generated secret key and PostgreSQL settings
+cat <<EOT > "$project_dir/.env"
+# Django settings
+DJANGO_SECRET_KEY='$DJANGO_SECRET_KEY'
+DJANGO_DEBUG=True
+DJANGO_ALLOWED_HOSTS=localhost,127.0.0.1
+
+# PostgreSQL settings
+POSTGRES_USER='your_postgres_user'
+POSTGRES_PASSWORD='your_postgres_password'
+POSTGRES_DB='your_database_name'
+POSTGRES_HOST='localhost'
+POSTGRES_PORT=5432
+EOT
+
+# Create .env.example with a blank secret key and database credentials
+cat <<EOT > "$project_dir/.env.example"
+# Django settings
+DJANGO_SECRET_KEY=''
+DJANGO_DEBUG=True
+DJANGO_ALLOWED_HOSTS=localhost,127.0.0.1
+
+# PostgreSQL settings
+POSTGRES_USER=''
+POSTGRES_PASSWORD=''
+POSTGRES_DB=''
+POSTGRES_HOST='localhost'
+POSTGRES_PORT=5432
+EOT
+
+# Step 3: Modify settings.py to read from .env file and configure PostgreSQL
+pip install python-dotenv psycopg2
+
+# Modify settings.py for environment variables and PostgreSQL configuration
+if [[ "$(uname -s)" == Darwin* ]]; then
+    sed -i '' "1s/^/import os\nfrom dotenv import load_dotenv\nload_dotenv()\n\n/" "$project_dir/$project_name/settings.py"
+    sed -i '' "/^SECRET_KEY/c\SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')" "$project_dir/$project_name/settings.py"
+    sed -i '' "/^DEBUG/c\DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'" "$project_dir/$project_name/settings.py"
+    sed -i '' "/^ALLOWED_HOSTS/c\ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')" "$project_dir/$project_name/settings.py"
+    # Remove existing DATABASES configuration
+    sed -i '' '/^DATABASES = {/,+5 d' "$project_dir/$project_name/settings.py"
+    # Add new PostgreSQL Database configuration
+    sed -i '' "/^# Database/a\
+        DATABASES = {\n\
+            'default': {\n\
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',\n\
+                'NAME': os.getenv('POSTGRES_DB', 'postgres'),\n\
+                'USER': os.getenv('POSTGRES_USER', 'postgres'),\n\
+                'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),\n\
+                'HOST': os.getenv('POSTGRES_HOST', 'localhost'),\n\
+                'PORT': os.getenv('POSTGRES_PORT', '5432'),\n\
+            }\n\
+        }\n\
+    " "$project_dir/$project_name/settings.py"
+else
+    sed -i "1s/^/import os\nfrom dotenv import load_dotenv\nload_dotenv()\n\n/" "$project_dir/$project_name/settings.py"
+    sed -i "/^SECRET_KEY/c\SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default-secret-key')" "$project_dir/$project_name/settings.py"
+    sed -i "/^DEBUG/c\DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'" "$project_dir/$project_name/settings.py"
+    sed -i "/^ALLOWED_HOSTS/c\ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')" "$project_dir/$project_name/settings.py"
+    # Remove existing DATABASES configuration
+    sed -i '/^DATABASES = {/,+5 d' "$project_dir/$project_name/settings.py"
+    # Add new PostgreSQL Database configuration
+    sed -i "/^# Database/a \
+        DATABASES = {\n\
+            'default': {\n\
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',\n\
+                'NAME': os.getenv('POSTGRES_DB', 'postgres'),\n\
+                'USER': os.getenv('POSTGRES_USER', 'postgres'),\n\
+                'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),\n\
+                'HOST': os.getenv('POSTGRES_HOST', 'localhost'),\n\
+                'PORT': os.getenv('POSTGRES_PORT', '5432'),\n\
+            }\n\
+        }\n\
+    " "$project_dir/$project_name/settings.py"
+fi
+
+
+# Freeze dependencies
+pip freeze > "$project_dir/requirements.txt"
+
+echo "Django project $project_name has been set up with PostgreSQL support | Project location - $project_dir"
+echo "Now committing changes to the git"
+
+git add .
+git commit -m "1.1.0/chore: Project Generated with PostgreSQL"
+
+# ASCII Art Success Message
+cat << "EOF"
+
+  _____                 _ _                
+ / ____|               | | |               
+| (___   ___  _ __   __| | |__  _   _  ___ 
+ \___ \ / _ \| '_ \ / _` | '_ \| | | |/ _ \
+ ____) | (_) | | | | (_| | |_) | |_| |  __/
+|_____/ \___/|_| |_|\__,_|_.__/ \__, |\___|
+                                 __/ |     
+                                |___/      
+
+EOF
+
+echo "Setup Successful!"
